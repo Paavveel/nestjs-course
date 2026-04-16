15b511ef1eead3af058b2893153940f742726d49
Paavveel <pa.tomilin@gmail.com>
2026-04-16 15:49:44 +0300

    feat: add pipe example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UsePipes, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { StringToLowercasePipe } from './common/pipes/string-to-lowercase.pipe';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UsePipes(StringToLowercasePipe)
+  @Post()
+  create(@Body('title') title: string): string {
+    return `Movie ${title}`;
   }
 }
