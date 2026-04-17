01f46a6b8a3091bded753539c254fc402c736d48
Paavveel <pa.tomilin@gmail.com>
2026-04-17 15:18:56 +0300

    feat: add interceptor example

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { logger } from './common/middleware/logger.middleware';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   app.use(logger);
 
